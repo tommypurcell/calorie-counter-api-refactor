8171e3706bcc9e53a91f821352c4c94a65511331
Tony Russo <ynotrusso@users.noreply.github.com>
2024-02-13 23:30:10 +0700

    query houses

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express'
 const router = Router()
+import db from '../db.js'
 
-router.get('/houses', (req, res) => {
-  res.json([
-    { id: 1, location: 'Bali' },
-    { id: 2, location: 'Koh Phangan' }
-  ])
+router.get('/houses', async (req, res) => {
+  let { rows } = await db.query('SELECT * FROM houses')
+  res.json(rows)
 })
 
-router.get('/houses/1', (req, res) => {
-  res.json({ id: 1, location: 'Bali' })
+router.get('/houses/1', async (req, res) => {
+  let { rows } = await db.query('SELECT * FROM houses WHERE house_id = 1')
+  res.json(rows[0])
 })
 
 export default router
