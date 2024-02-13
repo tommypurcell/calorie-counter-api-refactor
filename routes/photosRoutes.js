eb4e6a98cb355a7f8c1a74ad92da59cef5432038
Tony Russo <ynotrusso@users.noreply.github.com>
2024-02-13 23:34:15 +0700

    query photos

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express'
 const router = Router()
+import db from '../db.js'
 
-router.get('/photos', (req, res) => {
-  res.json([
-    { id: 1, url: 'url1' },
-    { id: 2, url: 'url2' }
-  ])
+router.get('/photos', async (req, res) => {
+  let { rows } = await db.query('SELECT * FROM houses_photos')
+  res.json(rows)
 })
 
-router.get('/photos/1', (req, res) => {
-  res.json({ id: 1, url: 'url1' })
+router.get('/photos/1', async (req, res) => {
+  let { rows } = await db.query('SELECT * FROM houses_photos')
+  res.json(rows[0])
 })
 
 export default router
