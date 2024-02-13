3f17c8338f0978b66c8848b97bf01f0d9f734e29
Tony Russo <ynotrusso@users.noreply.github.com>
2024-02-13 23:28:54 +0700

    import db url from secrets file

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 import pg from 'pg'
 const { Pool } = pg
+import { DBURL } from './secrets.js'
 
 // Database connection parameters
 const db = new Pool({
   ssl: {
     rejectUnauthorized: false
   },
-  connectionString: ''
+  connectionString: DBURL
 })
 
 export default db
