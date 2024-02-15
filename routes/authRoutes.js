21623972705bc286033a5cd7aceb46a1e7a20922
Tony Russo <ynotrusso@users.noreply.github.com>
2024-02-15 23:43:34 +0700

    make the signup route insert data

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express'
 const router = Router()
+import db from '../db.js'
 
-router.get('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
   try {
-    res.send('Hello from Signup')
+    const { rows } = await db.query(`
+      INSERT INTO users (first_name, last_name, email, password, picture)
+      VALUES ('${req.body.first_name}', '${req.body.last_name}', '${req.body.email}', '${req.body.password}', '${req.body.picture}')
+      RETURNING *
+    `)
+    res.json(rows[0])
   } catch (err) {
     res.json({ error: err.message })
   }
