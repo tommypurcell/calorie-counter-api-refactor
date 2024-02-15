bd102479623d07085378e8ee8ee2686fbe28b83c
Tony Russo <ynotrusso@users.noreply.github.com>
2024-02-15 22:47:21 +0700

    add houses filters query

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -4,7 +4,40 @@ import db from '../db.js'
 
 router.get('/houses', async (req, res) => {
   try {
-    let { rows } = await db.query('SELECT * FROM houses')
+    // build query base
+    let sqlquery = 'SELECT * FROM houses'
+    let filters = []
+    // add WHERE
+    if (
+      req.query.location ||
+      req.query.max_price ||
+      req.query.min_rooms ||
+      req.query.search
+    ) {
+      sqlquery += ' WHERE '
+    }
+    // add filters
+    if (req.query.location) {
+      filters.push(`location = '${req.query.location}'`)
+    }
+    if (req.query.max_price) {
+      filters.push(`price <= '${req.query.max_price}'`)
+    }
+    if (req.query.min_rooms) {
+      filters.push(`rooms >= '${req.query.min_rooms}'`)
+    }
+    if (req.query.search) {
+      filters.push(`description LIKE '%${req.query.search}%'`)
+    }
+    // array to string divided by AND
+    sqlquery += filters.join(' AND ')
+    // add ORDER BY
+    if (req.query.sort) {
+      sqlquery += ` ORDER BY ${req.query.sort} ${req.query.order || 'ASC'}`
+    }
+    // Run query
+    let { rows } = await db.query(sqlquery)
+    // Respond
     res.json(rows)
   } catch (err) {
     res.json({ error: err.message })
