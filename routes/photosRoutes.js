b8670c86a1cc2abd51a17b8dccf761ef4a43420f
Tony Russo <ynotrusso@users.noreply.github.com>
2024-02-18 18:22:52 +0700

    add patch route for photos

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -42,4 +42,30 @@ router.get('/photos/:photo_id', async (req, res) => {
   }
 })
 
+router.patch('/photos/:photo_id', async (req, res) => {
+  try {
+    let { house_id, photo, featured } = req.body
+    // Start building the SQL query
+    let sqlquery = `UPDATE houses_photos `
+    // Add SET if the req.body object is not empty
+    if (req.body) {
+      sqlquery += `SET `
+    }
+    // Iterate over the keys of the req.body object and add each key-value pair to the SQL query
+    for (let key in req.body) {
+      sqlquery += `${key} = '${req.body[key]}', `
+    }
+    // Remove the trailing comma and space from the SQL query
+    sqlquery = sqlquery.slice(0, -2)
+    // Add the WHERE clause to the SQL query
+    sqlquery += ` WHERE id = ${req.params.photo_id} RETURNING *`
+    // Execute the SQL query
+    let { rows } = await db.query(sqlquery)
+    // Send the response
+    res.json(rows[0])
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+})
+
 export default router
