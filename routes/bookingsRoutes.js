31cc92a8ad56cf386b99db9c900f85e1c5809df2
Tony Russo <ynotrusso@users.noreply.github.com>
2024-02-18 17:10:13 +0700

    add post route for bookings

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -2,6 +2,28 @@ import { Router } from 'express'
 const router = Router()
 import db from '../db.js'
 
+router.post('/bookings', async (req, res) => {
+  try {
+    let {
+      house_id,
+      user_id,
+      from_date,
+      to_date,
+      message,
+      price_night,
+      price_total
+    } = req.body
+    let { rows } = await db.query(`
+      INSERT INTO bookings (house_id, user_id, from_date, to_date, message, price_night, price_total)
+      VALUES ('${house_id}', '${user_id}', '${from_date}', '${to_date}', '${message}', ${price_night}, '${price_total}')
+      RETURNING *
+    `)
+    res.json(rows[0])
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+})
+
 router.get('/bookings', async (req, res) => {
   try {
     let sqlquery = 'SELECT * FROM bookings'
