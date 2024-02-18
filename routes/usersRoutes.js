4cfda0257ded7c49ca7fd0984dd0679c5a9b7d36
Tony Russo <ynotrusso@users.noreply.github.com>
2024-02-18 18:00:55 +0700

    add patch route for users

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -22,4 +22,30 @@ router.get('/users/:user_id', async (req, res) => {
   }
 })
 
+router.patch('/users/:user_id', async (req, res) => {
+  try {
+    let { first_name, last_name, email, password, picture } = req.body
+    // Start building the SQL query
+    let sqlquery = `UPDATE users `
+    // Add SET if the req.body object is not empty
+    if (req.body) {
+      sqlquery += `SET `
+    }
+    // Iterate over the keys of the req.body object and add each key-value pair to the SQL query
+    for (let key in req.body) {
+      sqlquery += `${key} = '${req.body[key]}', `
+    }
+    // Remove the trailing comma and space from the SQL query
+    sqlquery = sqlquery.slice(0, -2)
+    // Add the WHERE clause to the SQL query
+    sqlquery += ` WHERE user_id = ${req.params.user_id} RETURNING *`
+    // Execute the SQL query
+    let { rows } = await db.query(sqlquery)
+    // Send the response
+    res.json(rows[0])
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+})
+
 export default router
