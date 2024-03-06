199387f6c0827d61a8a50f19249320727fd01be8
Tony Russo <ynotrusso@users.noreply.github.com>
2024-03-06 20:20:06 +0700

    add more validation to signup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,26 +8,53 @@ import { jwtSecret } from '../secrets.js'
 
 router.post('/signup', async (req, res) => {
   try {
-    const { rowsSelect } = await db.query(`
+    // Required fields
+    if (!req.body.email) {
+      throw new Error('email is required')
+    }
+    if (!req.body.password) {
+      throw new Error('password is required')
+    }
+    if (!req.body.picture) {
+      throw new Error('picture is required')
+    }
+    if (!req.body.first_name) {
+      throw new Error('first_name is required')
+    }
+    if (!req.body.last_name) {
+      throw new Error('last_name is required')
+    }
+    // Other validation
+    if (req.body.password.length < 6) {
+      throw new Error('password must be minimum 6 characters long')
+    }
+    // Check duplicate user by email
+    const userExists = await db.query(`
       SELECT * FROM users WHERE email = '${req.body.email}'
     `)
-    if (rowsSelect.length) {
+    if (userExists.rows.length) {
       throw new Error('User with this email already exists!')
     }
+    // Hash password
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    // Save user
     const { rows } = await db.query(`
       INSERT INTO users (first_name, last_name, email, password, picture)
       VALUES ('${req.body.first_name}', '${req.body.last_name}', '${req.body.email}', '${hashedPassword}', '${req.body.picture}')
       RETURNING *
     `)
-    const user = rows[0]
+    let user = rows[0]
+    // Create token
     const token = jwt.sign(
       { user_id: user.user_id, email: user.email },
       jwtSecret
     )
     res.cookie('jwt', token)
-    res.json(rows[0])
+    // Compose response
+    delete user.password
+    // Respond
+    res.json(user)
   } catch (err) {
     res.json({ error: err.message })
   }
