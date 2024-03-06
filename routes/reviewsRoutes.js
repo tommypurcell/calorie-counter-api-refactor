a20fc90df567d7395e380bb48df32c00e98e840d
Tony Russo <ynotrusso@users.noreply.github.com>
2024-03-06 23:47:51 +0700

    add logic for reading reviews

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -49,21 +49,28 @@ router.post('/reviews', async (req, res) => {
 
 router.get('/reviews', async (req, res) => {
   try {
+    if (!req.query.house_id) {
+      throw new Error('house_id is required')
+    }
     let sqlquery = `
       SELECT reviews.*, users.first_name, users.last_name, users.picture FROM reviews
       LEFT JOIN users ON users.user_id = reviews.user_id
+      WHERE house_id = ${req.query.house_id}
+      ORDER BY date DESC
     `
-    if (req.query.house) {
-      sqlquery += ` WHERE house_id = ${req.query.house}`
-    }
-    sqlquery += ' ORDER BY date DESC'
     let { rows } = await db.query(sqlquery)
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric'
+    })
     let reviews = rows.map((r) => {
       r.author = {
         firstName: r.first_name,
         lastName: r.last_name,
         picture: r.picture
       }
+      r.date = formatter.format(new Date(r.date))
       delete r.first_name
       delete r.last_name
       delete r.picture
