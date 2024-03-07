2190d4cab7fae024baac71fa6c2be01a966afd1c
Tony Russo <ynotrusso@users.noreply.github.com>
2024-03-07 19:03:44 +0700

    add credentials to cors

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ import cors from 'cors'
 import 'dotenv/config'
 const app = express()
 
+app.use(cors({ origin: true, credentials: true }))
+app.use(express.json())
+app.use(cookieParser())
+
 import usersRoutes from './routes/usersRoutes.js'
 import housesRoutes from './routes/housesRoutes.js'
 import reviewsRoutes from './routes/reviewsRoutes.js'
 import bookingsRoutes from './routes/bookingsRoutes.js'
 import photosRoutes from './routes/photosRoutes.js'
 import authRoutes from './routes/authRoutes.js'
-
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
 
 app.use(usersRoutes)
 app.use(housesRoutes)
